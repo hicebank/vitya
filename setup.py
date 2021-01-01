0f6fb8f3c3d45f23f05f0917d35e8f7aa482f207
Dmitry <inyutin@hicebank.ru>
2021-01-02 00:32:11 +0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="vitya", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="hicebank.ru",
     author_email="levashov@hicebank.ru",
     description="validators for different russian banking values",
