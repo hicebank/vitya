4ae10a4d5af686d624666f1a704bbf85dc8468a2
Dmitry <inyutin@hicebank.ru>
2021-01-02 18:08:15 +0400

    done inn

---
+++ tests/test_vitya.py
@@ -0,0 +1,26 @@
+import pytest
+
+from vitya import validate_inn, ValidationError
+
+
+@pytest.mark.parametrize("inn", [
+    "3664069397", "302502032671", "7707083893", "7703206417", "771002344404"
+])
+def test_valid_inn(inn):
+    """No exception raise"""
+    assert validate_inn(inn) is None
+
+
+@pytest.mark.parametrize("inn", [
+    None,           # inn can't be None
+    3664069397,     # inn can't be nothing than str
+    302502032671,
+    "770708389",    # inn should be size of 10 or 12 chars
+    "77100234440",
+    "3664069398",   # wrong checksums
+    "302502032672",
+    "302502032681"
+])
+def test_wrong_inn(inn):
+    with pytest.raises(ValidationError):
+        validate_inn(inn)
--- vitya/__init__.py
+++ vitya/__init__.py
@@ -2,34 +2,47 @@ import re
 from typing import List
 
 
+class ValidationError(ValueError):
+    """
+    Exception that raises if passed data is invalid
+    """
+    pass
+
+
 def validate_inn(inn: str) -> None:
     """
-    TODO
+    Source:
+    https://www.consultant.ru/document/cons_doc_LAW_134082/947eeb5630c9f58cbc6103f0910440cef8eaccac/
+    https://ru.wikipedia.org/wiki/%D0%98%D0%B4%D0%B5%D0%BD%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D1%8B%D0%B9_%D0%BD%D0%BE%D0%BC%D0%B5%D1%80_%D0%BD%D0%B0%D0%BB%D0%BE%D0%B3%D0%BE%D0%BF%D0%BB%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%89%D0%B8%D0%BA%D0%B0
     """
     if not inn:
-        raise ValueError("inn is empty")
-    elif not re.fullmatch(r'[0-9]+', inn):
-        raise ValueError("inn can contain only numbers")
-    elif len(inn) not in [10, 12]:
-        raise ValueError("inn can consists only 10 or 12 numbers")
+        raise ValidationError("inn is empty")
 
-    def check_digits(inn: str, coefficients: List[int]) -> int:
-        n = 0
-        for idx, coefficient in enumerate(coefficients):
-            n += coefficient * int(inn[idx])
+    if not isinstance(inn, str):
+        raise ValidationError("inn should be passed as string")
+
+    if not re.fullmatch(r'[0-9]+', inn):
+        raise ValidationError("inn can contain only numbers")
+
+    def count_checksum(inn: str, coefficients: List[int]) -> int:
+        assert len(inn) >= len(coefficients)
+        n = sum([int(a) * b for a, b in zip(inn[:len(coefficients)], coefficients)])
         return n % 11 % 10
 
     if len(inn) == 10:
-        n10 = check_digits(inn, [2, 4, 10, 3, 5, 9, 4, 6, 8])
+        n10 = count_checksum(inn, [2, 4, 10, 3, 5, 9, 4, 6, 8])
         if n10 != int(inn[9]):
-            raise ValueError("wrong checksum")
+            raise ValidationError(f"wrong checksum on last digit: {inn[9]}; expected: {n10}")
         return
 
     if len(inn) == 12:
-        n11 = check_digits(inn, [7, 2, 4, 10, 3, 5, 9, 4, 6, 8])
-        n12 = check_digits(inn, [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8])
-        if n11 == int(inn[10]) and n12 == int(inn[11]):
-            raise ValueError("wrong checksum")
+        n11 = count_checksum(inn, [7, 2, 4, 10, 3, 5, 9, 4, 6, 8])
+        if n11 != int(inn[10]):
+            raise ValidationError(f"wrong checksum on pre-last digit: {inn[10]}; expected: {n11}")
+
+        n12 = count_checksum(inn, [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8])
+        if n12 != int(inn[11]):
+            raise ValidationError(f"wrong checksum on last digit: {inn[11]}; expected: {n12}")
         return
 
-    raise ValueError("wrong size of inn")
+    raise ValidationError("wrong size of inn, it can be 10 or 12 only")
