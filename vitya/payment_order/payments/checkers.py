93be2f990cf04e8959d3817c0ae7f4f1e4b91a55
pavel <p917910@gmail.com>
2023-05-15 16:27:06 +0300

    HB-4475

--- vitya/payment_order/payments/checkers.py
+++ vitya/payment_order/payments/checkers.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, ClassVar, Dict, List, Tuple, Type, cast, Sequence
+from typing import Any, ClassVar, Dict, List, Tuple, Type, Sequence
 
 from pydantic import BaseModel, root_validator
 from pydantic.errors import PydanticValueError
