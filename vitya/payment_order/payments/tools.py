e6ed5e5c30510c0370a93b0ededd16e4fa86be79
alaex777 <alaex777@gmail.com>
2023-12-22 16:54:31 +0300

    HB-5391

--- vitya/payment_order/payments/tools.py
+++ vitya/payment_order/payments/tools.py
@@ -5,9 +5,6 @@ IP_ACCOUNTS_PREFIXES_5 = {
     '40802', '45814', '45914', '47610', '47611', '47832',
     *[str(prefix) for prefix in range(42108, 42114 + 1)]
 }
-IP_ACCOUNTS_PREFIXES_3 = {
-    '454',
-}
 FL_ACCOUNTS_PREFIXES_5 = {
     '40803', '40810', '40813', '40817', '40820', '40824', '40826', '40827', '40828', '40914', '45815',
     '45817', '45915', '45917', '47411', '47468', '47603', '47605', '47608', '47609', '47833', '47835',
@@ -16,7 +13,7 @@ FL_ACCOUNTS_PREFIXES_3 = {
     '423', '426', '455', '457',
 }
 CHAMELEON_PREFIXES_5 = {
-    '40804', '40805', '40915', '40806', '40809', '40812',
+    '40804', '40805', '40915', '40806', '40809', '40812', '454',
     '40814', '40815', '40827', '40828', '40830', '40831', '40911',
 }
 
@@ -26,7 +23,6 @@ PREFIX_5_TO_ACCOUNT_KIND = {
     **{prefix: AccountKind.CHAMELEON for prefix in CHAMELEON_PREFIXES_5},
 }
 PREFIX_3_TO_ACCOUNT_KIND = {
-    **{prefix: AccountKind.IP for prefix in IP_ACCOUNTS_PREFIXES_3},
     **{prefix: AccountKind.FL for prefix in FL_ACCOUNTS_PREFIXES_3},
 }
 
